fb1d1e7bd41951fbb9b964374a7a4d97db86de2c
patison89 <patison89@gmail.com>
2018-04-27 21:12:48 +0100

    redirect after logging in

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router} from "@angular/router";
+import {CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {AuthService} from "./auth.service";
 
 @Injectable()
@@ -8,11 +8,11 @@ export class AuthGuardService implements CanActivate{
   constructor(private auth:AuthService,
               private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if(user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
       return false;
     })
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {AngularFireAuth} from "angularfire2/auth";
 import * as firebase from 'firebase';
 import {Observable} from "rxjs/Observable";
+import {ActivatedRoute} from "@angular/router";
 
 
 @Injectable()
@@ -9,11 +10,14 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth,
+              private route: ActivatedRoute) {
     this.user$ = this.afAuth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
   logout() {
