d98d4cc69c74ee1adc3ffa055e9d42e6d497cf50
patison89 <patison89@gmail.com>
2018-04-27 18:40:52 +0100

    use async pipe and user observable to display users name and correct navbar options

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from "angularfire2/auth";
+import * as firebase from 'firebase';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'bs-navbar',
@@ -7,9 +9,11 @@ import {AngularFireAuth} from "angularfire2/auth";
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent  {
+  user$: Observable<firebase.User>;
+
 
   constructor(private afAuth: AngularFireAuth) {
-
+      this.user$ = this.afAuth.authState;
   }
 
   logout() {
