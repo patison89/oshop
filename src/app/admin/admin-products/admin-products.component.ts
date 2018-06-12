e57b5ea88be333a1c9821bae14fe33a0ab200bb3
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-06-12 11:22:12 +0100

    display list of products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.products$ = this.productService.getAll();
   }
 
 }
