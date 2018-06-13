32e3ec7ee2b46fd5b8c925f462b546cd4bd8ab94
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-06-13 09:53:48 +0100

    implement data table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DataTableResource} from 'angular5-data-table';
 import {Subscription} from 'rxjs/Subscription';
 import {Product} from '../../models/product';
 import {ProductService} from '../../services/product.service';
@@ -12,11 +13,29 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: Product[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.subscription = this.productService.getAll().subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    });
+  }
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource<Product>(products);
+    this.tableResource.query({offset: 0})
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+  reloadItems(params) {
+    if (!this.tableResource) return;
+    this.tableResource.query(params)
+      .then(items => this.items = items);
   }
   filter(query: string) {
     console.log(query);
