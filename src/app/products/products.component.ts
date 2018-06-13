25bcdb034b80e9708daeaf946b59edb18f42ec37
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-06-13 11:31:56 +0100

    display list of categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoryService} from '../services/category.service';
 import {ProductService} from '../services/product.service';
 
 @Component({
@@ -8,9 +9,12 @@ import {ProductService} from '../services/product.service';
 })
 export class ProductsComponent implements OnInit {
   products$;
+  categories$;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService) {
     this.products$ = this.productService.getAll();
+    this.categories$ = this.categoryService.getAll();
   }
 
   ngOnInit() {
