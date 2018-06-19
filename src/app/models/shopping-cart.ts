b4e2f4c621bee45ba18b814d76bb503a3bdf73af
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-06-19 12:56:06 +0100

    refactor cart model

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,12 +1,14 @@
 import {ShoppingCartItem} from './shopping-cart-item';
 
 export class ShoppingCart {
+  items: ShoppingCartItem[] = [];
 
-  constructor(public items: ShoppingCartItem[]) {}
-
-  get productIds() {
-   return Object.keys(this.items);
+  constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
+    for(const productId in itemsMap) {
+      this.items.push(itemsMap[productId]);
+    }
   }
+
   get totalItemsCount() {
     let count = 0;
     for (const productId in this.items) {
