c67aab78b723d12053fb9b4bc23e66f9d7e1600d
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-06-19 13:04:31 +0100

    display price in cart

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -5,10 +5,17 @@ export class ShoppingCart {
 
   constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
     for(const productId in itemsMap) {
-      this.items.push(itemsMap[productId]);
+      const item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
     }
   }
-
+get totalPrice() {
+    let sum = 0;
+    for (const productId in this.items) {
+      sum += this.items[productId].totalPrice
+    }
+    return sum;
+}
   get totalItemsCount() {
     let count = 0;
     for (const productId in this.items) {
